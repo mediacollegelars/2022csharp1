be6a907a307db55f81b95f540b1824e4d0ad6888
Lars <36763@ma-web.nl>
2024-02-27 10:45:59 +0100

    1

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor
+                = ConsoleColor.Green;
+            string text = "Hello, World";
+
+            foreach (char letter in text)
+            {
+                Console.Write(letter);
+                Thread.Sleep(75);
+            }
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+        
+           
+
+            // Display current Foreground color 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
