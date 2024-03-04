cd5f7b055b5efa9243341a92599efd3b6238e21b
Lars <36763@ma-web.nl>
2024-03-04 12:52:06 +0100

    1

---
+++ Time/Time/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ prullenbak/prullenbak/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace prullenbak
+{
+    internal class Program
+    {
+
+    string prullenbak = "#FFFFFF"
+            bool = full
+            double a = 12.3;
+        bool ok = false; 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
